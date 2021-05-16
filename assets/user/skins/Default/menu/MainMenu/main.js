7969afccc563ec45d4ff1d0bce7ce3e121b3b684
Callum C <cpcooper34@hotmail.com>
2021-05-16 15:58:40 +1200

    feat: Added Settings JS App callbacks

--- assets/user/skins/Default/menu/MainMenu/main.js
+++ assets/user/skins/Default/menu/MainMenu/main.js
@@ -1,3 +1,18 @@
+//JS calls into C api
+function applySettings() {
+    PushSettings({ volume: 5, FpsCap: 60});
+}
+
+function cancelSettings() {
+    RevertSettings();
+}
+
+function selectSkin() {
+    SelectSkin();
+}
+
+
+//JS functions
 function display(element, hide, style) {
     for (let ele of hide) {
         ele.style.display = "none";
@@ -24,6 +39,18 @@ AudioHeader.addEventListener("click", () => display(
 
 SettingsQuit.addEventListener("click", () => display(MainMenu,
  document.body.children, "block"));
+
+VolumeSelect.onchange = function() {
+    VolumeSelectNumber.value = this.value;
+}
+
+VolumeSelectNumber.onchange = function() {
+    if (isNaN(this.value) || this.value > this.max || this.value < this.min) {
+        this.value = VolumeSelect.value;
+        return;
+    }
+    VolumeSelect.value = this.value;
+}
 
 //Initial listener calls
 DisplayHeader.click();
--- src/OnBeat/Ui/MainMenu/MainMenu.cpp
+++ src/OnBeat/Ui/MainMenu/MainMenu.cpp
@@ -17,29 +17,29 @@ namespace OnBeat
 		}
 
 		//Select file callback
-		JSValueRef SelectFile(JSContextRef ctx, JSObjectRef function,
+		JSValueRef SelectSkin(JSContextRef ctx, JSObjectRef function,
 			JSObjectRef thisObject, size_t argumentCount,
 			const JSValueRef arguments[], JSValueRef* exception)
 		{
-
 			return JSValueMakeNull(ctx);
 		}
 
 		//Apply settings callback
-		JSValueRef ApplySettings(JSContextRef ctx, JSObjectRef function,
+		JSValueRef PushSettings(JSContextRef ctx, JSObjectRef function,
 			JSObjectRef thisObject, size_t argumentCount,
 			const JSValueRef arguments[], JSValueRef* exception)
 		{
-
+			//Todo cast these to JSObject types using JSValueToObject then to JSValue using JSObjectGetProperty
+			//Then cast to ctypes and check for errors if they havent been passed
+			//Update the MainApp->Config with the new values
 			return JSValueMakeNull(ctx);
 		}
 
 		//Cancel settings callback
-		JSValueRef CancelSettings(JSContextRef ctx, JSObjectRef function,
+		JSValueRef RevertSettings(JSContextRef ctx, JSObjectRef function,
 			JSObjectRef thisObject, size_t argumentCount,
 			const JSValueRef arguments[], JSValueRef* exception)
 		{
-
 			return JSValueMakeNull(ctx);
 		}
 	}
@@ -60,15 +60,35 @@ namespace OnBeat
 		//Create JS Callbacks
 		ul::Ref<ul::JSContext>context = caller->LockJSContext();
 		JSContextRef ctx = context.get();
-
-		//Exit game callbacks
-		JSStringRef ExitGameName = JSStringCreateWithUTF8CString("ExitGame");
-		JSObjectRef func = JSObjectMakeFunctionWithCallback(ctx, ExitGameName,
-			JSCallbacks::ExitGame);
 		JSObjectRef globalObj = JSContextGetGlobalObject(ctx);
-		JSObjectSetProperty(ctx, globalObj, ExitGameName, func, 0, 0);
+
+		//Exit game callback
+		JSStringRef ExitGameName = JSStringCreateWithUTF8CString("ExitGame");
+		JSObjectRef EGfunc = JSObjectMakeFunctionWithCallback(ctx, ExitGameName,
+			JSCallbacks::ExitGame);
+		JSObjectSetProperty(ctx, globalObj, ExitGameName, EGfunc, 0, 0);
 		JSStringRelease(ExitGameName);
 
+		//Apply settings callback
+		JSStringRef PushSettingsName = JSStringCreateWithUTF8CString("PushSettings");
+		JSObjectRef PSfunc = JSObjectMakeFunctionWithCallback(ctx, PushSettingsName,
+			JSCallbacks::PushSettings);
+		JSObjectSetProperty(ctx, globalObj, PushSettingsName, PSfunc, 0, 0);
+		JSStringRelease(PushSettingsName);
+
+		//Revert settings callback
+		JSStringRef RevertSettingsName = JSStringCreateWithUTF8CString("RevertSettings");
+		JSObjectRef RSfunc = JSObjectMakeFunctionWithCallback(ctx, RevertSettingsName,
+			JSCallbacks::RevertSettings);
+		JSObjectSetProperty(ctx, globalObj, RevertSettingsName, RSfunc, 0, 0);
+		JSStringRelease(RevertSettingsName);
+
+		//Select skin callback
+		JSStringRef SelectSkinName = JSStringCreateWithUTF8CString("SelectSkin");
+		JSObjectRef SSfunc = JSObjectMakeFunctionWithCallback(ctx, SelectSkinName,
+			JSCallbacks::SelectSkin);
+		JSObjectSetProperty(ctx, globalObj, SelectSkinName, SSfunc, 0, 0);
+		JSStringRelease(SelectSkinName);
 	}
 
 	MainMenu::~MainMenu()
